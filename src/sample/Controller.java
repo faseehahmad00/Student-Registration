a96dc2fd874ec9ac87a90778ab41aed8f8f0c4b3
faseehahmad00 <faseehahmad00@gmail.com>
2019-12-02 17:20:04 +0500

    commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,18 +1,14 @@
 package sample;
-
-
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.w3c.dom.events.Event;
 
 import java.awt.event.ActionEvent;
@@ -46,80 +42,84 @@ public class Controller {
     TextField cnic = new TextField();
     @FXML
     Text errormsg = new Text();
+@FXML
+    ComboBox<String> comboBox = new ComboBox<>();
 
-    public Controller() throws IOException {
+    public void setComboBox(ComboBox<String> comboBox) {
+        this.comboBox = comboBox;
+        comboBox.getItems().addAll("apple","mango","ORANGE");
     }
+
+    public Controller() throws IOException {}
+    Stage controlstage = new Stage();
 
 ///LOGIN FORM START==================================================================================================
-    public void onexitclick() {
-        Platform.exit();
-    }
-
-    public void onclick() throws Exception {
-        String passcode = "admin";
-        String username = "admin";
-        if (passcode.equals(passwordField.getText()) && username.equals(textField.getText())) {
-
-            System.out.println("login successfull");
-            //opening Registration form using login form
-            Parent root1 = FXMLLoader.load(getClass().getResource("menu.fxml"));
-            Stage stage1 = new Stage();
-            stage1.setTitle("WELCOME TO STUDENT REGISTRATION");
-            stage1.setScene(new Scene(root1, 550, 500));
-            stage1.show();
-//            stage1.setFullScreen(true);
-        } else if (textField.getText().isEmpty()) {
-            textField.setPromptText("Enter username");
-        } else if (passwordField.getText().isEmpty()) {
-            passwordField.setPromptText("Enter password");
-        } else {
-            System.out.println("Invalid username or password");
-            errormsg.setText(">>> invalid username or password");
+        public void onexitclick () {
+            Platform.exit();
         }
 
+        public void onclick () throws Exception {
+            String passcode = "admin";
+            String username = "admin";
+            if (passcode.equals(passwordField.getText()) && username.equals(textField.getText())) {
 
-    }
+                System.out.println("login successfull");
+                //opening Registration form using login form
+                Parent root1 = FXMLLoader.load(getClass().getResource("menu.fxml"));
+                controlstage.setTitle("WELCOME TO STUDENT REGISTRATION");
+                controlstage.setScene(new Scene(root1));
+                controlstage.show();
+                controlstage.setFullScreen(true);
+            } else if (textField.getText().isEmpty()) {
+                textField.setPromptText("Enter username");
+            } else if (passwordField.getText().isEmpty()) {
+                passwordField.setPromptText("Enter password");
+            } else {
+                System.out.println("Invalid username or password");
+                errormsg.setText(">>> invalid username or password");
+            }
+
+
+        }
 //LOGIN FORM END========================================================================================================
 
 ///MENU BAR START=======================================================================================================
-    public void add() throws IOException {
-        //opening Registration form using login form
-        Stage stage2 = new Stage();
-        Parent root = FXMLLoader.load(getClass().getResource("regform.fxml"));
-        stage2 = new Stage();
-        stage2.setTitle("WELCOME TO STUDENT REGISTRATION");
-        stage2.setScene(new Scene(root, 550, 500));
-        stage2.show();
-//        stage2.setFullScreen(true);
-    }
-    public void menucancel(){
+        public void add () throws IOException {
+            //opening Registration form using login form
+            controlstage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("regform.fxml"));
+            controlstage.setTitle("WELCOME TO STUDENT REGISTRATION");
+            controlstage.setScene(new Scene(root));
+            controlstage.show();
+            controlstage.setFullScreen(true);
+        }
+        public void menucancel () {
 
-        Platform.exit();
-    }
+            Platform.exit();
+        }
 //MENU BAR END==========================================================================================================
 
 //REGISTRATION FORM START===============================================================================================
-    public void oncancel() throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
-        Stage stage3 = new Stage();
-        stage3.setTitle("WELCOME TO STUDENT REGISTRATION");
-        stage3.setScene(new Scene(root, 550, 500));
-        stage3.show();
-//        stage3.setFullScreen(true);
-    }
+        public void oncancel () throws IOException {
+            Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+            controlstage.setTitle("WELCOME TO STUDENT REGISTRATION");
+            controlstage.setScene(new Scene(root));
+            controlstage.show();
+            controlstage.setFullScreen(true);
+        }
 
-    public void onsaveclicked() { //on pressing save button on registration form.
-        System.out.println(ID.getText());
-        System.out.println(name.getText());
-        System.out.println(fname.getText());
-        System.out.println(gender.getText());
-        System.out.println(cnic.getText());
-        System.out.println(dob.getText());
-        System.out.println(phone.getText());
-        System.out.println(address.getText());
-        System.out.println(degree.getText());
-        System.out.println(email.getText());
-    }
+        public void onsaveclicked () { //on pressing save button on registration form.
+            System.out.println(ID.getText());
+            System.out.println(name.getText());
+            System.out.println(fname.getText());
+            System.out.println(gender.getText());
+            System.out.println(cnic.getText());
+            System.out.println(dob.getText());
+            System.out.println(phone.getText());
+            System.out.println(address.getText());
+            System.out.println(degree.getText());
+            System.out.println(email.getText());
+        }
 //REGISTRATION FORM END=================================================================================================
 
-}
+    }
