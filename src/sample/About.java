91c2e1cbf1e690eb04851bce9e22cd2e13ce2977
oyeahmed <ahmed.shabbir1308@gmail.com>
2019-12-15 01:58:57 +0500

    About.fxml update

--- src/sample/About.java
+++ src/sample/About.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class About {
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class About implements Initializable {
+    @FXML
+    private Label label;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TranslateTransition tt = new TranslateTransition(Duration.seconds(4), label);
+        tt.setToX(label.getLayoutX());
+        tt.play();
+    }
 }
